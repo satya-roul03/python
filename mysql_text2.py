6bea4bcb65596aec1498c78a8dc8a8cfcb0977ce
Satyajit Roul <149993938+satya-roul03@users.noreply.github.com>
2024-03-07 14:18:13 +0530

    Create mysql_text2.py

---
+++ mysql_text2.py
@@ -0,0 +1,23 @@
+import pymysql
+try:
+    con=pymysql.connect(host="localhost",database="student",user="root",password="Satya111@")
+    cur=con.cursor()
+    query="delete from s"
+    cur.execute(query)
+    con.commit()
+    query="select *from s"
+    cur.execute(query)
+    con.commit()
+    data=cur.fetchone()
+    while data is not None:
+        print(data)
+        data=cur.fetchone()
+except pymysql.DatabaseError as e:
+    if con:
+        con.rollback()
+        print("The problem is : ",e)
+finally:
+    if cur:
+        cur.close()
+    if con:
+        con.close()
